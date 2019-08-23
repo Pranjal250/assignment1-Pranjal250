dd7b3ea3899a47200fb7b1c8446fb706826b353e
pranjal <pranjal.jain_cs18@gla.ac.in>
2019-08-24 00:22:34 +0530

    add variables:speed,distance,time add scanner class to take input

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -10,6 +10,13 @@ import java.util.Scanner;
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
+        double speed;
+        double distance;
+        double time;
+        double g = 9.8;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the time");
+        time = sc.nextInt();
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
